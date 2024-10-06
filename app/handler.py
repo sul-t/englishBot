d0ba9ae46c905a03fc88ab80415237c0151fc5c8
sultan <153429280+sul-t@users.noreply.github.com>
2024-10-06 18:20:14 +0500

    дополнения функционала исправлены ошибки с генерацией диапазонов слов добавлен hard node режим перевода слов добавлены две реплай клавиши "Билет" - отвечающая за выбор билета и "Режим" - переключает режим ответа пользователя

--- app/handler.py
+++ app/handler.py
@@ -4,27 +4,50 @@ from typing import Any, Callable, Dict, Awaitable
 from aiogram import Router, F, BaseMiddleware
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, CallbackQuery, TelegramObject
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
 import app.keyboard as kb
+
 
 
 async def send_words(message, user_id):
     if cursor.execute('select user_id from users where user_id = ?', (user_id,)).fetchone() is None:
         cursor.execute('insert into users(user_id) values(?)', (user_id,))
 
-    word_en_id, word_en, part_of_speech, translation, keyboard = await kb.generate_words(user_id)
+    word_en_id, word_en, part_of_speech, keyboard = await kb.generate_words(user_id)
 
-    cursor.execute('update users set word_id = ?, word = ? where user_id = ?', (word_en_id, translation, user_id))
+    cursor.execute('update users set word_id = ? where user_id = ?', (word_en_id, user_id))
 
-    await message.answer(f'🔍 Выберите перевод слова <strong><i>{word_en}</i></strong>. 📝 Часть речи — <strong><i>{part_of_speech}</i></strong>', reply_markup=keyboard, parse_mode='HTML')
+    if cursor.execute('select hard_mode from users where user_id = ?', (user_id,)).fetchone()[0]:
+        await message.answer(f'🔍 Выберите перевод слова <strong><i>{word_en}</i></strong>. 📝 Часть речи — <strong><i>{part_of_speech}</i></strong>', parse_mode='HTML')
+    else:
+        await message.answer(f'🔍 Выберите перевод слова <strong><i>{word_en}</i></strong>. 📝 Часть речи — <strong><i>{part_of_speech}</i></strong>', reply_markup=keyboard, parse_mode='HTML')
 
     connect.commit()
 
 
-router = Router()
+async def processUserAnswer(text, message, user_id):
+    word_en_id = cursor.execute('select word_id from users where user_id = ?', (user_id,)).fetchone()[0]
+    word_en, part_of_speech = cursor.execute('select word, part_of_speech from words where id = ?',(word_en_id,)).fetchone()
+    words_ru = cursor.execute('select translation from translations where word_id = ?', (word_en_id,)).fetchall()
 
-connect = sqlite3.connect('./english.db')
-cursor = connect.cursor()
+    list_ru_words = ', '.join(word[0] for word in words_ru)
 
+    if (text).lower() == 'не знаю':
+        await message.answer(f'📚 Английское слово: <b>{word_en}</b>\n📝 Часть речи: <b>{part_of_speech}</b>\n🌍 Перевод слова: <b>{list_ru_words}</b>', parse_mode='HTML')
+
+        return await send_words(message, user_id)
+
+    row = cursor.execute('select translation from translations where word_id = (select word_id from users where user_id = ?)', (user_id,)).fetchall()
+    
+    rigth_words = [word[0] for word in row]
+
+    if text.lower() not in rigth_words:
+        await message.answer(f'❌ Неверно!\n📚 Английское слово: <b>{word_en}</b>\n📝 Часть речи: <b>{part_of_speech}</b>\n🌍 Перевод слова: <b>{list_ru_words}</b>', parse_mode='HTML')
+    else:
+        await message.answer('Верно!')
+
+    return await send_words(message, user_id)
 
 
 def isdigit(text):
@@ -34,46 +57,69 @@ def isdigit(text):
         return None
 
 
-@router.message(Command('bilet'))
-async def choice_bilet(message: Message):
-    await message.answer('🎟️ Выберите номер билета от 0 до 200.\n📩 Отправьте -1, чтобы учить все слова сразу!')
+
+router = Router()
+
+connect = sqlite3.connect('./english.db')
+cursor = connect.cursor()
+
+
+
+class biletDialogStates(StatesGroup):
+    number_bilet = State()
+
+
+
+@router.message(CommandStart())
+async def start(message: Message):
+    if cursor.execute('select user_id from users where user_id = ?', (message.from_user.id,)).fetchone() is None:
+        cursor.execute('insert into users(user_id) values(?)', (message.from_user.id,))
+        connect.commit()
+
+    await message.answer('Изучай английские слова с помощью данного бота', reply_markup=kb.reply_keyboard())
+
+
+@router.message(F.text == 'Билет')
+async def choose_bilet(message: Message, state: FSMContext):
+    await message.answer('🎟️ Отправьте число от 1 до 200, оно будет соответствовать номеру билета.\n📩 Отправьте 0, чтобы учить все слова сразу!', reply_markup=kb.reply_keyboard())
+    await state.set_state(biletDialogStates.number_bilet)
+
+@router.message(F.text, biletDialogStates.number_bilet)
+async def update_chosen_bilet(message: Message, state: FSMContext):
+    number_bilet = isdigit(message.text)
+
+    if (number_bilet is not None) and (0 <= number_bilet <= 200):
+        await message.answer(f'Вы решаете {number_bilet} билет')
+        cursor.execute('update users set bilet = ? where user_id = ?', (number_bilet, message.from_user.id))
+        connect.commit()
+
+    await send_words(message, message.from_user.id)
+
+
+@router.message(F.text == 'Режим')
+async def change_mode(message: Message):
+    hard_mode = cursor.execute('update users set hard_mode = not hard_mode returning hard_mode').fetchone()[0]
+    connect.commit()
+ 
+    await message.answer(f'Ваш режим был изменен на {'сложный' if hard_mode else 'простой'}!')
+    await send_words(message, message.from_user.id)
 
 
 @router.message()
-async def main(msg: Message):
-    text = isdigit(msg.text)
+async def main(message: Message):
+    if cursor.execute('select hard_mode from users where user_id = ?', (message.from_user.id,)).fetchone()[0]:
+        return await processUserAnswer(message.text, message, message.from_user.id)
 
-    if (text is not None) and (-1 <= text < 200):
-        await msg.answer(f'Вы решаете {text} билет')
-        cursor.execute('update users set bilet = ? where user_id = ?', (text, msg.from_user.id))
-        connect.commit()
-    
-    await send_words(msg, msg.from_user.id)
+    await send_words(message, message.from_user.id)
 
 
 @router.callback_query()
 async def check_response(callback: CallbackQuery):
     await callback.answer()
 
-    word_en_id = cursor.execute('select word_id from users where user_id = ?', (callback.from_user.id,)).fetchone()[0]
-    word_en, part_of_sheech = cursor.execute('select word, part_of_speech from words where id = ?',(word_en_id,)).fetchone()
-    words_ru = cursor.execute('select translation from translations where word_id = ?', (word_en_id,)).fetchall()
-
-    list_ru_words = ', '.join(word[0] for word in words_ru)
+    await processUserAnswer(callback.data, callback.message, callback.from_user.id)
 
  
-    if callback.data == 'Не знаю':
-        await callback.message.answer(f'📚 Английское слово: <b>{word_en}</b>\n📝 Часть речи: <b>{part_of_sheech}</b>\n🌍 Перевод слова: <b>{list_ru_words}</b>', parse_mode='HTML')
-
-        return await send_words(callback.message, callback.from_user.id)
-
-    word = cursor.execute('select word from users where user_id = ?', (callback.from_user.id,)).fetchone()[0]
-
-    if word != callback.data:
-        await callback.message.answer(f'❌ Неверно!\n📚 Английское слово: <b>{word_en}</b>\n📝 Часть речи: <b>{part_of_sheech}</b>\n🌍 Перевод слова: <b>{list_ru_words}</b>', parse_mode='HTML')
-    else:
-        await callback.message.answer('Верно!')
-
-    return await send_words(callback.message, callback.from_user.id)
+    
 
 
--- app/keyboard.py
+++ app/keyboard.py
@@ -1,6 +1,6 @@
 import sqlite3, random
 
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
@@ -8,25 +8,30 @@ connect = sqlite3.connect('./english.db')
 cursor = connect.cursor()
 
 
+def generate_word_id(bilet):
+    range_random = bilet * 50
+
+    if bilet == 0:
+        right_word_id = random.randint(1, 9998)
+    elif bilet == 200:
+        right_word_id = random.randint(range_random - 49, range_random - 2)
+    else:
+        right_word_id = random.randint(range_random - 49, range_random)
+
+    return right_word_id
+
+
 async def generate_words(user_id):
     keyboard = InlineKeyboardBuilder()
 
-    row = cursor.execute('select bilet from users where user_id = ?', (user_id,)).fetchone()
+    bilet = cursor.execute('select bilet from users where user_id = ?', (user_id,)).fetchone()[0]
 
-    if row is None:
-        bilet = -1
-    else:
-        bilet = row[0]
-
-    if bilet == -1:
-        right_word_id = random.randint(1, 9998)
-    else:
-        right_word_id = random.randint(bilet, bilet + 50)
-
+    right_word_id = generate_word_id(bilet)
 
     word_en, word_en_id, part_of_speech = cursor.execute('select word, id, part_of_speech from words where id = ?', (right_word_id,)).fetchone()
     translations = cursor.execute('select translation from translations where word_id = ?', (right_word_id,)).fetchall()
-    translation = translations[random.randint(0, len(translations) - 1)][0]
+    index = random.randint(0, len(translations) - 1) if len(translations) != 1 else 0
+    translation = translations[index][0]
 
     answers = set([translation])
 
@@ -35,16 +40,14 @@ async def generate_words(user_id):
         bad_word_id = 0
 
         while True:
-            if bilet == -1:
-                bad_word_id = random.randint(1, 9998)
-            else:
-                bad_word_id = random.randint(bilet, bilet + 50)
+            bad_word_id = generate_word_id(bilet)
 
             if bad_word_id not in answers:
                 break
             
         words_ru = cursor.execute('select translation from translations where word_id = ?', (bad_word_id,)).fetchall()
-        word_ru = words_ru[random.randint(0, len(words_ru) - 1)][0]
+        index = random.randint(0, len(words_ru) - 1) if len(words_ru) != 1 else 0
+        word_ru = words_ru[index][0]
 
         answers.add(word_ru)
 
@@ -58,6 +61,14 @@ async def generate_words(user_id):
     for row in answers:
         keyboard.add(InlineKeyboardButton(text=row, callback_data=row))
 
-    return word_en_id, word_en, part_of_speech, translation, keyboard.adjust(2).as_markup()
+    return word_en_id, word_en, part_of_speech, keyboard.adjust(2).as_markup()
 
 
+def reply_keyboard():
+    button = [
+        [KeyboardButton(text='Билет'), KeyboardButton(text='Режим')]
+    ]
+
+    keyboard = ReplyKeyboardMarkup(keyboard=button, resize_keyboard=True)
+
+    return keyboard
